c20aae82f931ae92d46f74f59e9f2ce9146b9c22
Ghoza <ghoza60@gmail.com>
2025-02-25 19:56:40 +0700

    Update page.tsx

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -95,6 +95,7 @@ export default function TokenSwap() {
             (Number(price) * fromAmount) / Number(decimal)
           }`
         : isReading;
+        // fromAmount * decimal
     }
   };
 
